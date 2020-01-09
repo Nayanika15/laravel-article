c781be9ab78c73a36bf444cf155ef8fc9369bd72
nayanika15 <nayanikapanigrahi1115@gmail.com>
2020-01-09 16:59:39 +0530

    Added laravel-mix

--- resources/views/layouts/wordify.blade.php
+++ resources/views/layouts/wordify.blade.php
@@ -8,17 +8,7 @@
 
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300, 400,700|Inconsolata:400,700" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
-
-    <link rel="stylesheet" href="{{ asset('fonts/ionicons/css/ionicons.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('fonts/fontawesome/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('fonts/flaticon/font/flaticon.css') }}">
-    
-    <!-- Theme Style -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/styleError.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,700" rel="stylesheet">
     
@@ -47,19 +37,19 @@
     <!-- loader -->
     <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#f4b214"/></svg></div>
 
-    <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
-    <script src="{{ asset('js/jquery-migrate-3.0.0.js') }}"></script>
+    <script src="{{ mix('/js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ mix('/js/jquery-migrate-3.0.0.js') }}"></script>
     
-    <script src="{{ asset('js/popper.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
-    <script src="{{ asset('js/jquery.stellar.min.js') }}"></script>
+    <script src="{{ mix('/js/popper.min.js') }}"></script>
+    <script src="{{ mix('/js/bootstrap.min.js') }}"></script>
+    <script src="{{ mix('/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ mix('/js/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ mix('/js/jquery.stellar.min.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/additional-methods.min.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-   <script src="{{ asset('js/main.js') }}"></script>
-   <script src="{{ asset('js/validatorFile.js') }}" ></script>
+   <script src="{{ mix('/js/main.js') }}"></script>
+   <script src="{{ mix('/js/validatorFile.js') }}" ></script>
    @yield('scripts')
   </body>
     </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,29 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-	.copy('public/templates/wordify/js/bootstrap.min.js', 'public/js/bootstrap.min.js');
+mix.js('public/templates/wordify/js/main.js', 'public/js/main.js')
+	.copy('public/templates/wordify/js/bootstrap.min.js', 'public/js/bootstrap.min.js')
+	.copy('public/templates/wordify/js/jquery-3.2.1.min.js', 'public/js/jquery-3.2.1.min.js')
+	.copy('public/templates/wordify/js/jquery-migrate-3.0.0.js', 'public/js/jquery-migrate-3.0.0.js')
+	.copy('public/templates/wordify/js/jquery.stellar.min.js', 'public/js/jquery.stellar.min.js')
+	.copy('public/templates/wordify/js/jquery.waypoints.min.js', 'public/js/jquery.waypoints.min.js')
+	.copy('public/templates/wordify/js/owl.carousel.min.js', 'public/js/owl.carousel.min.js')
+	.copy('public/templates/wordify/js/popper.min.js', 'public/js/popper.min.js')
+	.scripts('public/templates/wordify/js/validatorFile.js', 'public/js/validatorFile.js')
+	.copyDirectory('public/templates/wordify/images', 'public/images')
+	.styles([
+		'public/templates/wordify/css/style.css',
+		'public/templates/wordify/css/animate.css',
+		'public/templates/wordify/css/bootstrap.css',
+		'public/templates/wordify/css/flaticon.css',
+		'public/templates/wordify/css/owl.carousel.min.css',
+		'public/templates/wordify/css/style.css',
+		'public/templates/wordify/css/styleError.css',
+		'public/templates/wordify/fonts/ionicons/css/ionicons.min.css',
+		'public/templates/wordify/fonts/fontawesome/css/font-awesome.min.css',
+		'public/templates/wordify/fonts/flaticon/font/flaticon.css'
+		], 'public/css/app.css')
+		.options({
+	      processCssUrls: false
+	   	})
+	.copyDirectory('public/templates/wordify/fonts', 'public/fonts');
